710fcbe19c9f1a6834ced73f15fdf6ab7621c37d
E-2uan <ranlaroc002@gmail.com>
2025-04-19 00:19:58 +0700

    Update Volume.cs

--- Assets/Menu/code Menu/Volume.cs
+++ Assets/Menu/code Menu/Volume.cs
@@ -9,10 +9,26 @@ public class Volume : MonoBehaviour
     public AudioMixer AudioMix;
     public Slider musicSlider;
     public Slider soundSlider;
-   
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("musicVolume"))
+        {
+            LoadMusicVol(); 
+        }
+          
+        else setMusicVol();
+    }
     public void setMusicVol()
     {
         AudioMix.SetFloat("MusicVol",musicSlider.value);
+        PlayerPrefs.SetFloat("musicVolume", musicSlider.value);
+    }
+
+    public void LoadMusicVol()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        setMusicVol();
     }
     public void setSoundvol()
     {
